7aaf26a4d05505d2faf1221ae79a0ccf36d8b41b
Kyza <kyza@kyza.net>
2021-03-18 15:40:05 -0400

    Test before push smh

--- injector/removeCSP.js
+++ injector/removeCSP.js
@@ -3,13 +3,18 @@ logger.log("Removing CSP.");
 
 const { session } = require("electron");
 
+const headers = [
+	"content-security-policy-report-only",
+	"content-security-policy",
+	"x-frame-options",
+];
+
 session.defaultSession.webRequest.onHeadersReceived((details, callback) => {
-	// Only delete the CSP headers if they exist.
-	if (details.responseHeaders["content-security-policy-report-only"]) {
-		delete details.responseHeaders["content-security-policy-report-only"];
-	}
-	if (details.responseHeaders["content-security-policy"]) {
-		delete details.responseHeaders["content-security-policy"];
+	// Only delete the headers if they exist.
+	for (const header of headers) {
+		if (details.responseHeaders[header]) {
+			delete details.responseHeaders[header];
+		}
 	}
 	callback({ cancel: false, responseHeaders: details.responseHeaders });
 });
