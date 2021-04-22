30a53b17f925a6e02331109d5cb6d38421bebded
Kyza <kyza@kyza.net>
2021-04-21 23:41:36 -0400

    JSDocs testing

--- core/index.js
+++ core/index.js
@@ -1,2 +1,2 @@
-export { * as logger } from "kernel/logger";
-export { * as utilities } from "kernel/utilities";
+export * as logger from "kernel/logger";
+export * as utilities from "kernel/utilities";
