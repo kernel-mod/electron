95b6b93e4341c57eaa6b9ed5a11e5fdae5d7c9c7
Kyza <kyza@kyza.net>
2021-04-22 12:12:02 -0400

    First core JSDocs

--- core/utilities/index.js
+++ core/utilities/index.js
@@ -1,6 +1,13 @@
-import * as electron from 'electron';
+/**
+ * @module utilities
+ */
 
-export function processLocation() {
+import * as electron from "electron";
+
+export /**
+ * A universal context function for identifying the context your code is running in.
+ * @returns {string("MAIN","PRELOAD","RENDERER")} The name of the current process.
+ */ function processLocation() {
 	if (!electron) {
 		return "RENDERER";
 	} else if (electron?.ipcMain) {
@@ -11,7 +18,13 @@ export function processLocation() {
 	return "UNKNOWN";
 }
 
-export async function injectRendererModule({ path, sync = false, onload }) {
+export /**
+ * A function for the PRELOAD or RENDERER that injects a new separate module into the renderer.
+ * @param {object} options
+ * @param {string} options.path The path to the module.
+ * @param {boolean} [options.sync=false] Whether to run synchronously or asynchronously.
+ * @param {function} [options.onload=undefined] Code to run once the script has finished loading.
+ */ async function injectRendererModule({ path, sync = false, onload }) {
 	if (processLocation() === "MAIN") {
 		throw new Error("No.");
 	}
