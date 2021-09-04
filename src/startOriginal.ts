a78b5f1b13071d25c98240f8c4df9100ec1539d9
Kyza <kyza@kyza.net>
2021-09-03 21:04:36 -0400

    Non-asar

--- src/startOriginal.ts
+++ src/startOriginal.ts
@@ -1,21 +1,26 @@
 import * as path from "path";
 import * as electron from "electron";
+import * as fs from "fs";
 
 // We can get the path of the real injection point like this.
 const basePath = path.join(path.dirname(require.main.filename), "..");
-const asarPath = path.join(basePath, "app.asar");
+let originalPath = path.join(basePath, "app.asar");
+// Check if originalPath file exists.
+if (!fs.existsSync(originalPath)) {
+	originalPath = path.join(basePath, "app-original");
+}
 
 const originalPackage = require(path.resolve(
-	path.join(asarPath, "package.json")
+	path.join(originalPath, "package.json")
 ));
 
-const startPath = path.join(asarPath, originalPackage.main);
+const startPath = path.join(originalPath, originalPackage.main);
 
 // Don't corrupt the app on update.
 require.main.filename = startPath;
 
 // Load the app's original code.
 // @ts-ignore
-electron.app.setAppPath?.(asarPath);
+electron.app.setAppPath?.(originalPath);
 electron.app.name = originalPackage.name;
 import(startPath);
